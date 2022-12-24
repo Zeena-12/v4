4b9e05889809bfc5bffcae562e428fb916584189
mzh5167 <202007655@stu.uob.edu.bh>
2022-12-24 14:52:37 +0300

    Fixed issue with 'apply' validation

--- valide-filter.js
+++ valide-filter.js
@@ -5,21 +5,19 @@ const max = document.getElementById('max');
 
 //Event listener
 form.addEventListener('submit', function(e) {
-    e.preventDefault();
 
     let allError = 0;
     allError += isEmpty([min, max]);
 
-
-    if (allError == 0)
-        form.submit();
+    if (allError != 0)
+        e.preventDefault();
 
 });
 
 function isEmpty(inputArr) {
 
     let error = 0;
-    inputArr.forEach(function(input) {
+    inputArr.forEach(function (input) {
         const formControl = input.parentElement;
 
         if (input.value.trim() === '') {
